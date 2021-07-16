7971d88453e5c5d0319c8e51fd3729d586a45661
Appscorico <87219263+digitalnus@users.noreply.github.com>
2021-07-16 04:37:41 -0400

    Create install-license.bat
    Added batch file for installing license for MS Project 2019

---
+++ MS Project 2019/install-license.bat
@@ -0,0 +1,6 @@
+if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16"
+if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+cscript ospp.vbs /inpkey:B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B
+cscript ospp.vbs /inpkey:C4F7P-NCP8C-6CQPT-MQHV9-JXD2M
+cscript ospp.vbs /sethst:kms.teevee.asia
+cscript ospp.vbs /act
